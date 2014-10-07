84806e97111f63c256449d1ad4aa0cb2f56b540a
Muemin.Goezenoglu <Muemin.Goezenoglu@IT2028061.bfarm.de>
2014-10-07 12:23:49 +0200

    commit me

--- test/src/com/example/test/MainActivity.java
+++ test/src/com/example/test/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends ActionBarActivity {
 	 * new comment by MG
 	 * one more comment by MG
 	 * more and more
+	 * more
 	 * @see android.support.v7.app.ActionBarActivity#onCreate(android.os.Bundle)
 	 */
 
