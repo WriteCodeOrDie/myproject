a9800e40bf16f56eb9b888a769418e3deeb9bcfb
Arthur.Lier <Arthur.Lier@IT2025872.bfarm.de>
2014-10-08 10:57:40 +0200

    Commit Mittwoch

---
+++ ContactTool/src/de/projekt11/View/Main_Activity.java
@@ -0,0 +1,20 @@
+package de.projekt11.View;
+
+
+import com.example.contacttool.R;
+import android.app.Activity;
+import android.os.*;
+
+
+public class Main_Activity extends Activity {
+	
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_activity);
+		
+	}
+	
+
+}
